a742c290350bb741b86eabc4d8b75d0362070366
lostars <746320542jn@gmail.com>
2025-02-21 19:15:41 +0800

    social email hidden

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const user = document.getElementById('emailUser').getAttribute('value');
-    const domain = document.getElementById('emailDomain').getAttribute('value');
-
-    const email = atob(user) + "@" + atob(domain)
-    const mailtoLink = document.getElementById('emailLink');
-    mailtoLink.href = 'mailto:' + email;
+    const emailUser = document.getElementById('emailUser');
+    const emailDomain = document.getElementById('emailDomain');
+    if (emailUser && emailDomain) {
+        const user = emailUser.getAttribute('value');
+        const domain = emailDomain.getAttribute('value');
+        const email = atob(user) + "@" + atob(domain)
+        const mailtoLink = document.getElementById('emailLink');
+        mailtoLink.href = 'mailto:' + email;
+    }
 });
